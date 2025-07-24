67811ec77ad196e87bc7e7daa2fcaf4c96fc525f
RexDoniz <rexd2oniz@gmail.com>
2025-07-23 18:53:27 -0600

    segundo commit sitio REXDEVELOP vite vue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'url'
 export default defineConfig({
   plugins: [vue()],
   base: '/LandingPageVue3/',
+  resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
